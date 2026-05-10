a706f4408b88b232e4ecc7eca414fd6dd5a26862
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2026-05-10 18:04:08 +0530

    base setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # agent_loop_langchain_tool
+
+# how to run 
+source .venv_1/bin/activate
+uv run --active python main.py
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from agent-loop-langchain-tool!")
+
+
+if __name__ == "__main__":
+    main()
